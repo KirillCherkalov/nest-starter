1ac2f9a07f22ecc23283e17b4874ced73e677acf
Konstantin Tatarchuk <konstantin.tatarchuk@effective-soft.com>
2019-11-01 14:36:10 +0300

    rename config.module.ts -> module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from './config/config.module';
+import { ConfigModule } from './config/module';
 
 @Module({
   imports: [UsersModule, AuthModule, ConfigModule],
