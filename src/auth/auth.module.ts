43f0c4c6aabac7c7f9f4afb64e8a9ce504afc3a0
Andrei ES <andrei.vakulski@effective-soft.com>
2020-07-27 11:54:09 +0300

    Make JWT token expiration time configurable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -23,7 +23,7 @@ import { JwtStrategy } from './strategies/jwt';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.JWT_SECRET,
-        signOptions: { expiresIn: '600s' },
+        signOptions: { expiresIn: configService.JWT_EXPIRES_IN },
       }),
       inject: [ConfigService],
     }),
