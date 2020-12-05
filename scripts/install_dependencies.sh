07660f23b29de887e8b6133972259e5dc4af4748
Kirill Cherkalov <kirill.cherkalov@effective-soft.com>
2020-12-05 23:19:52 +0300

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,10 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm -v
 npm install pm2@latest -g
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+sudo apt-get install jq
 
 
 environmentVariables=$(aws ssm get-parameters-by-path --path /nest-server/development --query "Parameters[*].{Name:Name,Value:Value}")
