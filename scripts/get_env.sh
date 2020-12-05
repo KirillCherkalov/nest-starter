fc71d771c0417a61c6a0e0eedafb001842583603
Kirill Cherkalov <kirill.cherkalov@effective-soft.com>
2020-12-05 22:31:46 +0300

    added env script file

---
+++ scripts/get_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+environmentVariables=$(aws ssm get-parameters-by-path --path /nest-server/development --query "Parameters[*].{Name:Name,Value:Value}")
+
+#remove temp file for storing json if it exist
+[ -e env.json ] && rm env.json
+
+echo $environmentVariables >> env.json
+
+jq -c '.[]' env.json | while read i; do
+    varName=$(echo $i | jq -r '.Name')
+    varName=${varName##*/}  # retain the part after the last slash
+
+    varValue=$(echo $i | jq -r '.Value')
+    
+    echo $varName
+    echo $varValue
+
+    echo $varName=$varValue>> /var/www/nest-starter/.env
+done
+
+rm env.json
