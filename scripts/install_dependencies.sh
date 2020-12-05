4cb64425a18f7cbecd9f9db02c5ffde76721d2d4
Kirill Cherkalov <kirill.cherkalov@effective-soft.com>
2020-12-05 23:00:13 +0300

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,3 +5,25 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm -v
 npm install pm2@latest -g
+
+
+environmentVariables=$(aws ssm get-parameters-by-path --path /nest-server/development --query "Parameters[*].{Name:Name,Value:Value}")
+
+#remove temp file for storing json if it exist
+[ -e env.json ] && rm env.json
+
+echo $environmentVariables >> env.json
+
+jq -c '.[]' env.json | while read i; do
+    varName=$(echo $i | jq -r '.Name')
+    varName=${varName##*/}  # retain the part after the last slash
+
+    varValue=$(echo $i | jq -r '.Value')
+    
+    echo $varName
+    echo $varValue
+
+    echo $varName=$varValue>> /var/www/nest-starter/.env
+done
+
+rm env.json
